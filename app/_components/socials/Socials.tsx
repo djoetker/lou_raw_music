b2df7dec06d11237e7a1d849630709e0dae5f59a
djoetker <jansiede@posteo.de>
2025-03-12 16:27:43 +0100

    wip round fix

--- app/_components/socials/Socials.tsx
+++ app/_components/socials/Socials.tsx
@@ -10,7 +10,7 @@ export default function Socials() {
         <div key={index} className="flex flex-col flex-wrap items-center justify-center mb-1 w-5/6 max-w-[350px] h-fit mt-[1rem]">
 
           {Object.entries(links).map(([platform, url], idx) => (
-            <div key={idx} className="relative w-full ">
+            <div key={idx} className="relative w-full rounded-sm">
               <span className="absolute top-0 left-0.5 text-[0.6rem]">{section}</span>
               <a href={url} target="_blank" rel="noopener noreferrer" className="block bg-[#f5f5f5] bg-opacity-70 text-black border border-solid border-black rounded-sm hover:bg-slate-100 py-[0.75rem] px-[1.5rem] mb-[1rem] text-center w-full max-w-[400px]	">
                 {cleanUnderscore(platform)}
