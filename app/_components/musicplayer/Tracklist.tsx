d2b6925e01fcc75aa7348553e00e46c163c9270e
djoetker <jansiede@posteo.de>
2025-03-17 17:16:42 +0100

    tracklist looks

--- app/_components/musicplayer/Tracklist.tsx
+++ app/_components/musicplayer/Tracklist.tsx
@@ -1,10 +1,11 @@
 import TracklistLoading from "./_loading/TracklistLoading";
 import { cleanUnderscore } from "../../api/other/cleanUnderscore.js";
-
+import { useAudio } from '../../_context/audioContext';
 
 import React from "react";
 
 export default function Tracklist({ files, selectForPlayer, isLoading }: { files: any[]; selectForPlayer: (fileId: string) => void; isLoading: boolean }) {
+  const { currentTrackId } = useAudio();
 
   if (isLoading) {
     return (
@@ -12,12 +13,21 @@ export default function Tracklist({ files, selectForPlayer, isLoading }: { files
     );
   } else {
     return (
-      <div className='h-fit rounded-sm'>
-        {files.map(file => (
-          <div className="flex justify-start items-start w-fit p-[0.125rem] h-fit cursor-pointer" key={file.key} onClick={() => selectForPlayer(file.key)} >
-            <p className='text-black'>{cleanUnderscore(file.key.slice(0, -4))}</p>
-          </div>
-        ))}
+      <div className="h-fit rounded-sm">
+        {files.map((file, index) => {
+          const isSelected = file.key === currentTrackId;
+          return (
+            <div
+              key={index}
+              onClick={() => selectForPlayer(file.key)}
+              className={`flex justify-start items-start w-100 p-[0.125rem] h-fit cursor-pointer transition-colors duration-200
+                ${isSelected ? "bg-black text-white" : index % 2 === 0 ? "bg-gray-100" : "bg-gray-200"}
+              `}
+            >
+              <p>{cleanUnderscore(file.key.slice(0, -4))}</p>
+            </div>
+          );
+        })}
       </div>
     );
   };
